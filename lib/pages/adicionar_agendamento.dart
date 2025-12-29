acdf6f0bbef7c248fb7a315fb0ec276826ae753b
Gabriel Leal <gabriellealdeoliveira2005@gmail.com>
2025-12-29 11:49:39 -0300

    Adicionado o botao q vai levar para a tela de adicionar agendamento

---
+++ lib/pages/adicionar_agendamento.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class AdicionarAgendamento extends StatefulWidget{
+  AdicionarAgendamento({Key? key}) : super(key:key);
+  
+  @override
+  State<StatefulWidget> createState() => _AdicionarAgendamentoState();
+}
+
+class _AdicionarAgendamentoState extends State<AdicionarAgendamento>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
--- lib/pages/agendamentos_page.dart
+++ lib/pages/agendamentos_page.dart
@@ -1,3 +1,4 @@
+import 'package:agendamentos_mobile_dart/pages/adicionar_agendamento.dart';
 import 'package:agendamentos_mobile_dart/repositorys/agendamento_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -19,36 +20,44 @@ class _AgendamentosPageState extends State<AgendamentosPage> {
   late List<Agendamento> agendamentos =
       agendamentoRepository.agendamentos; //recebe os agendamentos da API
 
-  //métodos
-  mostrarDetalhesAgendamento(Agendamento agendamento) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) =>
-            MostrarDetalhesAgendamento(agendamento: agendamento),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Agendamentos'), centerTitle: true),
-      body:
-      agendamentos.isEmpty ? Center(child: Text('Nenhum agendamento no momento!',style: TextStyle(fontSize: 20))) :
-      ListView.builder(
-        itemCount: agendamentos.length,
-        itemBuilder: (context, index) {
-          final agendamento = agendamentos[index];
+      body: agendamentos.isEmpty
+          ? Center(
+              child: Text(
+                'Nenhum agendamento no momento!',
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          : ListView.builder(
+              itemCount: agendamentos.length,
+              itemBuilder: (context, index) {
+                final agendamento = agendamentos[index];
 
-            return ListTile(
-              title: Text(agendamento.cliente.nome),
-              subtitle: Text(agendamento.dataFormatada),
-              leading: const Icon(Icons.calendar_month),
-              trailing: const Icon(Icons.chevron_right),
-              onTap: () => mostrarDetalhesAgendamento(agendamento),
-            );
-        },
+                return ListTile(
+                  title: Text(agendamento.cliente.nome),
+                  subtitle: Text(agendamento.dataFormatada),
+                  leading: const Icon(Icons.calendar_month),
+                  trailing: const Icon(Icons.chevron_right),
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          MostrarDetalhesAgendamento(agendamento: agendamento),
+                    ),
+                  ),
+                );
+              },
+            ),
+
+      //botao flutuante, o botao de adicionar agendamento
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => AdicionarAgendamento()),
+        ),
       ),
     );
   }
